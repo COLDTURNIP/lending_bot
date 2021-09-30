81e6d1ce04bdc48045bb8588ea5d688b5b8fbd02
Raphanus Lo <coldturnip@gmail.com>
2021-09-30 16:09:53 +0800

    separate config exs by environment

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,3 @@
 import Config
 
-config :ftx_lending_bot, FtxLendingBot.Scheduler,
-  # Reference: https://github.com/quantum-elixir/quantum-core
-  jobs: [
-    {"*/15 * * * *", {FtxLendingBot.Lender, :call, []}}
-  ]
+import_config "#{Mix.env}.exs"
---
+++ config/dev.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :ftx_lending_bot, FtxLendingBot.Scheduler,
+  # Reference: https://github.com/quantum-elixir/quantum-core
+  jobs: [
+    # no job defined here
+  ]
